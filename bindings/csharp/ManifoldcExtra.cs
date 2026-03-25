b170e7f80310cff435715b8ecc907c4a59b20c58
Eli Aloni <aloni.eli@gmail.com>
2026-03-25 22:57:58 +0200

    fix iOS crash

--- bindings/csharp/ManifoldcExtra.cs
+++ bindings/csharp/ManifoldcExtra.cs
@@ -9,7 +9,11 @@ namespace Manifold
     public static unsafe partial class Manifoldc
     {
 
+        #if __IOS__
+        [DllImport("@rpath/manifoldc.framework/manifoldc", EntryPoint = "manifold_warp_internal", CallingConvention = CallingConvention.Cdecl)]
+        #else
         [DllImport("manifoldc", EntryPoint = "manifold_warp_internal", CallingConvention = CallingConvention.Cdecl)]
+        #endif
         private static extern IntPtr manifold_warp_internal(void* mem, IntPtr m, IntPtr fun, void* ctx);
         public static IntPtr manifold_warp(void* mem, IntPtr m, delegate* unmanaged<ManifoldVec3*, double, double, double, void*, void> fun, void* ctx) => manifold_warp_internal(mem, m, (IntPtr)fun, ctx);
 
